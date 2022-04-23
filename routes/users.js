aa7cd61699e9abf3dfc98d4682c9fc3e558c5655
Serhii Horoshko <sr.horoshko@gmail.com>
2022-04-23 21:55:51 +0300

    Patch: Added: pm i joi-password-complexity/

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ export const user = express.Router();
 user.post('/', async (req, res) => {
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+  if (!req.body.password) return res.status(400).send('Password is required.');
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send('User already registered.');
